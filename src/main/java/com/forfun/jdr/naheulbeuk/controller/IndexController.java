40c25b7ed2c29bbc43182a0f01fcb693e1ffb198
deathwall <gerar1501@gmail.com>
2018-10-28 21:46:33 +0100

    debug
    vidage liste perso si rechargement acceuil

--- src/main/java/com/forfun/jdr/naheulbeuk/controller/IndexController.java
+++ src/main/java/com/forfun/jdr/naheulbeuk/controller/IndexController.java
@@ -26,6 +26,7 @@ public class IndexController {
     
     @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
     public String index(Model model) {
+    	persons.clear();
     	persons.addAll(playerService.getAllPlayers());
         return "index";
     }
